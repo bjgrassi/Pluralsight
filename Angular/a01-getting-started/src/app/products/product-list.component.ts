934dbe9e01ed1cf49feea58b5a921dafa144d9d9
Bruna Grassi <bruna.grassi@soulasalle.com.br>
2020-07-11 11:51:32 -0300

    More on Components

--- Angular/a01-getting-started/src/app/products/product-list.component.ts
+++ Angular/a01-getting-started/src/app/products/product-list.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { IProduct } from './product';
 
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle = 'Product List';
   imageWidth = 50;
   imageMargin = 2;
   showImage = false;
-  listFilter = 'cart';
-  products: any[] = [
+
+  _listFilter = ''; //Backing Field (Campo de Apoio). Generally is private
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  filteredProducts: IProduct[] = []; //New variable to preserve the original array informations
+  products: IProduct[] = [
     {
       "productId": 1,
       "productName": "Leaf Rake",
@@ -33,7 +46,22 @@ export class ProductListComponent {
     }
   ]
 
+  constructor() { //Set default values for more complex properties
+    this.filteredProducts = this.products;
+    this._listFilter = 'cart'
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
   }
 }
