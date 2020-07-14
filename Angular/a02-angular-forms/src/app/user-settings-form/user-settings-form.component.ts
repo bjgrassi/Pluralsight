80ea75099ec5e8d9629b36c990e5bcb047546582
Bruna Grassi <bruna.grassi@soulasalle.com.br>
2020-07-13 21:35:37 -0300

    HTTP Form Posting and Data Access

--- Angular/a02-angular-forms/src/app/user-settings-form/user-settings-form.component.ts
+++ Angular/a02-angular-forms/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
 import { NgForm, NgModel } from '@angular/forms';
+import { DataService } from '../data/data.service';
+import { IfStmt } from '@angular/compiler';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -19,10 +22,14 @@ export class UserSettingsFormComponent implements OnInit {
 
   //Protecting data. EX: user navigates to other page or hit the cancel button
   userSettings: UserSettings = { ...this.originalUserSettings };
+  postError = false;
+  postErrorMessage = '';
+  subscriptionTypes: Observable<string[]>
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.subscriptionTypes = this.dataService.getSubscriptionTypes();
   }
 
   onBlur(field: NgModel) {
@@ -31,5 +38,21 @@ export class UserSettingsFormComponent implements OnInit {
 
   onSubmit(form: NgForm) {
     console.log("onSubmit: ", form.valid)
+
+    if(form.valid) {
+      this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+        result => console.log('success: ', result),
+        error => this.onHttpError(error)
+      );
+    } else {
+      this.postError = true;
+      this.postErrorMessage = "Please, fix the above errors."
+    }
+  }
+
+  onHttpError(errorResponse: any) {
+    console.log('error: ', errorResponse);
+    this.postError = true;
+    this.postErrorMessage = errorResponse.error.errorMessage
   }
 }
