6c883960f55b4752c0cd0a31542cccd5177ce028
Bruna Grassi <bruna.grassi@soulasalle.com.br>
2020-07-16 21:11:37 -0300

    Creating and Communicating Between Angular Components (@Input())

--- Angular/a04-angular-fundamentals/src/app/app.module.ts
+++ Angular/a04-angular-fundamentals/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventTumbnailComponent } from './events/event-tumbnail.component';
+import { NavbarComponent } from './nav/navbar.component';
 
 @NgModule({
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventTumbnailComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
