b2bcfbe20fdd92bbc34e2713ee16bdd690fcb08e
Bruna Grassi <bruna.grassi@soulasalle.com.br>
2020-07-17 19:07:24 -0300

    Routing and Navigating Pages (matching id paramns, 404 page, canActivate and canDeactivate [helps user not cancel an action, because of the popup dialog])

--- Angular/a04-angular-fundamentals/src/app/app.module.ts
+++ Angular/a04-angular-fundamentals/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
@@ -7,21 +8,38 @@ import { EventTumbnailComponent } from './events/event-tumbnail.component';
 import { NavbarComponent } from './nav/navbar.component';
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { appRoutes } from './routes';
+import { CreateEventComponent } from './events/create-event.component';
+import { Error404Component } from './error/404.component';
+import { EventRouteActivatorService } from './events/event-details/event-route-activator.service';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventTumbnailComponent,
-    NavbarComponent
+    EventDetailsComponent,
+    NavbarComponent,
+    CreateEventComponent,
+    Error404Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     EventService,
-    ToastrService
+    ToastrService,
+    EventRouteActivatorService,
+    { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState }
   ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if(component.isDirty)
+    return window.confirm('ddd')
+  return true
+}
