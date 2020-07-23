57126c8c25667e19b6cf67a97ec3da6ef98de689
Bruna Grassi <bruna.grassi@soulasalle.com.br>
2020-07-23 18:55:08 -0300

    Prefetching Data Using Route Resolvers (loading all info before show the page)

--- Angular/a05-angular-routing/src/app/products/product.module.ts
+++ Angular/a05-angular-routing/src/app/products/product.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductResolver } from './product-resolver.service';
 
 import { SharedModule } from '../shared/shared.module';
 
@@ -11,15 +12,26 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', component: ProductDetailComponent },
-      { path: 'products/:id/edit', component: ProductEditComponent }
-    ])
+      {
+        path: 'products',
+        component: ProductListComponent,
+      },
+      {
+        path: 'products/:id',
+        component: ProductDetailComponent,
+        resolve: { resolvedData: ProductResolver },
+      },
+      {
+        path: 'products/:id/edit',
+        component: ProductEditComponent,
+        resolve: { resolvedData: ProductResolver },
+      },
+    ]),
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
-  ]
+    ProductEditComponent,
+  ],
 })
-export class ProductModule { }
+export class ProductModule {}
