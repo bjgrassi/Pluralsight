c8365f3e8c8f0674bfc8a0e825bf42129ba136a0
Bruna Grassi <bruna.grassi@soulasalle.com.br>
2020-07-23 19:59:59 -0300

    Grouping and Component-less Routes

--- Angular/a05-angular-routing/src/app/products/product.module.ts
+++ Angular/a05-angular-routing/src/app/products/product.module.ts
@@ -16,21 +16,26 @@ import { SharedModule } from '../shared/shared.module';
     RouterModule.forChild([
       {
         path: 'products',
-        component: ProductListComponent,
-      },
-      {
-        path: 'products/:id',
-        component: ProductDetailComponent,
-        resolve: { resolvedData: ProductResolver }
-      },
-      {
-        path: 'products/:id/edit',
-        component: ProductEditComponent,
-        resolve: { resolvedData: ProductResolver },
         children: [
-          { path: '', redirectTo: 'info', pathMatch: 'full' },
-          { path: 'info', component: ProductEditInfoComponent },
-          { path: 'tags', component: ProductEditTagsComponent }
+          {
+            path: '',
+            component: ProductListComponent
+          },
+          {
+            path: ':id',
+            component: ProductDetailComponent,
+            resolve: { resolvedData: ProductResolver }
+          },
+          {
+            path: ':id/edit',
+            component: ProductEditComponent,
+            resolve: { resolvedData: ProductResolver },
+            children: [
+              { path: '', redirectTo: 'info', pathMatch: 'full' },
+              { path: 'info', component: ProductEditInfoComponent },
+              { path: 'tags', component: ProductEditTagsComponent }
+            ]
+          }
         ]
       }
     ])
