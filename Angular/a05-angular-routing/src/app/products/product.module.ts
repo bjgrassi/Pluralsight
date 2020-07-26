934c9652027fe6df2bf9ca8961b95ec5b7ab1378
Bruna Grassi <bruna.grassi@soulasalle.com.br>
2020-07-26 13:58:01 -0300

    Lazy Loading

--- Angular/a05-angular-routing/src/app/products/product.module.ts
+++ Angular/a05-angular-routing/src/app/products/product.module.ts
@@ -9,37 +9,30 @@ import { ProductEditTagsComponent } from './product-edit/product-edit-tags.compo
 import { ProductResolver } from './product-resolver.service';
 
 import { SharedModule } from '../shared/shared.module';
-import { AuthGuard } from '../user/auth.guard';
-import { ProductEditGuard } from '../products/product-edit/product-edit.guard';
+import { ProductEditGuard } from './product-edit/product-edit.guard';
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
       {
-        path: 'products',
-        canActivate: [AuthGuard],
+        path: '',
+        component: ProductListComponent
+      },
+      {
+        path: ':id',
+        component: ProductDetailComponent,
+        resolve: { resolvedData: ProductResolver }
+      },
+      {
+        path: ':id/edit',
+        component: ProductEditComponent,
+        canDeactivate: [ProductEditGuard],
+        resolve: { resolvedData: ProductResolver },
         children: [
-          {
-            path: '',
-            component: ProductListComponent
-          },
-          {
-            path: ':id',
-            component: ProductDetailComponent,
-            resolve: { resolvedData: ProductResolver }
-          },
-          {
-            path: ':id/edit',
-            component: ProductEditComponent,
-            canDeactivate: [ProductEditGuard],
-            resolve: { resolvedData: ProductResolver },
-            children: [
-              { path: '', redirectTo: 'info', pathMatch: 'full' },
-              { path: 'info', component: ProductEditInfoComponent },
-              { path: 'tags', component: ProductEditTagsComponent }
-            ]
-          }
+          { path: '', redirectTo: 'info', pathMatch: 'full' },
+          { path: 'info', component: ProductEditInfoComponent },
+          { path: 'tags', component: ProductEditTagsComponent }
         ]
       }
     ])
