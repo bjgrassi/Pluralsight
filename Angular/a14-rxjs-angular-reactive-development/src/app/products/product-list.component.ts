05ea85d9579af2e01084c2d05d197748ca8d0e80
Bruna Grassi <bruna.grassi@soulasalle.com.br>
2020-08-26 18:17:12 -0300

    Reacting to Actions: Examples

--- Angular/a14-rxjs-angular-reactive-development/src/app/products/product-list.component.ts
+++ Angular/a14-rxjs-angular-reactive-development/src/app/products/product-list.component.ts
@@ -12,13 +12,14 @@ import { ProductCategoryService } from '../product-categories/product-category.s
 })
 export class ProductListComponent {
   pageTitle = 'Product List';
-  errorMessage = '';
+  private errorMessageSubject = new Subject<string>();
+  errorMessage$ = this.errorMessageSubject.asObservable();
 
   private categorySelectedSubject = new BehaviorSubject<number>(0);
   categorySelectedAction$ = this.categorySelectedSubject.asObservable();
 
   products$ = combineLatest([
-    this.productService.productsWithCategory$,
+    this.productService.productsWithAdd$,
     this.categorySelectedAction$
   ])
     .pipe(
@@ -27,7 +28,7 @@ export class ProductListComponent {
           selectedCategoryId ? product.categoryId === selectedCategoryId : true
         )),
       catchError(err => {
-        this.errorMessage = err;
+        this.errorMessageSubject.next(err);
         return EMPTY;
       })
     );
@@ -35,7 +36,7 @@ export class ProductListComponent {
   categories$ = this.productCategoryService.productCategories$
     .pipe(
       catchError(err => {
-        this.errorMessage = err;
+        this.errorMessageSubject.next(err);
         return EMPTY;
       })
     );
@@ -44,7 +45,7 @@ export class ProductListComponent {
               private productCategoryService: ProductCategoryService) { }
 
   onAdd(): void {
-    console.log('Not yet implemented');
+    this.productService.addProduct();
   }
 
   onSelected(categoryId: string): void {
